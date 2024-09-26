8bfee6c540dafa3e9b854d57cda4c731183e5cb6
wnsdnl <andyjun03@naver.com>
2024-09-26 19:21:57 +0900

    mul, div feature added

--- calculator.py
+++ calculator.py
@@ -4,7 +4,15 @@ def add(x, y):
 def sub(x, y):
     return x - y
 
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
 num1, num2 = map(float, input("두 수를 입력하시오: ").split())
 
 print(add(num1, num2))
 print(sub(num1, num2))
+print(mul(num1, num2))
+print(div(num1, num2))
